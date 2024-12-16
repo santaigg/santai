2656cdd76e0581875f9e545cbe97c6f3e77c84c8
Kadir Lofca <kadirlofca@outlook.com>
2024-12-16 13:15:31 -0500

    made room for stats on right side of home page

---
+++ src/components/information/TopThree.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { fetchLeaderboard } from "../../utils/fetch/fetchLeaderboard";
+  import { type LeaderboardId } from "../../utils/types/leaderboard";
+
+  const leaderboard = await fetchLeaderboard("season0" as LeaderboardId);
+</script>
